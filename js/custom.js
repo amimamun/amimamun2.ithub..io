b47bbe557dfa0c8c2df1dc960dbd9af3995eecc0
amimamun <31954341+amimamun@users.noreply.github.com>
2017-09-21 01:31:30 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+$(".homepage-slides").owlcarousel({
+	items: 1,
+	loop: true,
+	nav: true,
+	dots: true,
+	navText:["<i clss='fa fa-long-arrow-left'></i>", "<i clss='fa fa-long-arrow-right'></i>"],
+	autoplay:false,
+
+
+});
+}) ;
